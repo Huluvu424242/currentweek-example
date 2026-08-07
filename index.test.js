8ca7c2e9aabb5a60243fe6e68aa9aa9670d2c5bc
huluvu424242 <huluvu424242@gmail.com>
2026-08-07 20:58:48 +0200

    Umstellung auf ES Module

--- index.test.js
+++ index.test.js
@@ -1,5 +1,17 @@
-const weekNum = require('./index.js');
+import getWeekNumber from './index.js'
 
-test('that weekNum returns a value', () => {
-  expect(weekNum).not.toBeNull();
-});
+describe('getWeekNumber', () => {
+  test('is exported as a function', () => {
+    expect(getWeekNumber).toBeInstanceOf(Function)
+  })
+
+  test('returns a number for the current date', () => {
+    expect(getWeekNumber()).toEqual(expect.any(Number))
+  })
+
+  test('returns ISO week 32 for 07.08.2026', () => {
+    const date = new Date(2026, 7, 7)
+
+    expect(getWeekNumber(date)).toBe(32)
+  })
+})
